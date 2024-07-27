10d25908d9a8087e23d4d29a9677b1e4100d1284
L2405833156 <jun999ty2002f@icloud.com>
2024-07-27 12:08:55 +0900

    Your commit message

--- src/main/java/raiseteach/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raiseteach/StudentManagement/StudentManagementApplication.java
@@ -1,21 +1,42 @@
 package raiseteach.StudentManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	@Autowired
+	private StudentRepository repository;
+
+	private String name = "Enami Koji";
+	private String age = "37";
+
+
+
 	public static void main(String[] args) {
-		//localhost:8080/
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
-@GetMapping("/hang")
-	public String hello()
-{
-	return "eat,fish";
+
+	@GetMapping("/name")
+	public String getName(@RequestParam String name) {
+		Student student = repository.searchByName(name);
+		return student.getName()+" "+student.getAge()+"歳";
+	}
+
+	@PostMapping("/name")
+	public void setName(String name, int age) {
+		repository.registerStudent(name, age);
+	}
+
+	@PostMapping("/updatename")
+	public void upName(String name) {
+		this.name = name;
+	}
 }
-}
+
